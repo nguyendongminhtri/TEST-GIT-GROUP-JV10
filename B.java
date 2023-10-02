7df1303feb31b04c104eb539e57d4862b0bd275a
nguyendongminhtri <nguyendongminhtri29092014@gmail.com>
2023-10-02 23:09:55 +0700

    Push Lần 2

--- B.java
+++ B.java
@@ -1,5 +1,6 @@
 public class B {
     public static void main(String[] args) {
         System.out.println("Thêm vào B");
+        System.out.println("Thêm vào B1");
     }
 }
--- C.java
+++ C.java
@@ -1,5 +1,6 @@
 public class C {
     public static void main(String[] args) {
         System.out.println("Đây là C");
+        System.out.println("Thêm vao C1");
     }
 }
