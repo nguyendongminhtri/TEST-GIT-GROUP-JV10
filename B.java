f7e1fc2e12aaaba60359be146adb7108d056ef0a
nguyendongminhtri <nguyendongminhtri29092014@gmail.com>
2023-10-02 23:15:10 +0700

    Revert "Push Lần 2"
    This reverts commit 7df1303feb31b04c104eb539e57d4862b0bd275a.

--- B.java
+++ B.java
@@ -1,6 +1,5 @@
 public class B {
     public static void main(String[] args) {
         System.out.println("Thêm vào B");
-        System.out.println("Thêm vào B1");
     }
 }
--- C.java
+++ C.java
@@ -1,6 +1,5 @@
 public class C {
     public static void main(String[] args) {
         System.out.println("Đây là C");
-        System.out.println("Thêm vao C1");
     }
 }
